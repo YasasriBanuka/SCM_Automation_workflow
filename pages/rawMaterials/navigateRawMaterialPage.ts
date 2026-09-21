bfa631743155e293abb1e7b95096a6920c292e01
YasasriBanuka <banukadias5@gmail.com>
2026-09-21 15:10:50 +0530

    Add navigation to Raw Materials section

---
+++ pages/rawMaterials/navigateRawMaterialPage.ts
@@ -0,0 +1,36 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class navigateRawMaterialPage {
+
+    readonly page: Page;
+    readonly lblRawMaterial: Locator;
+    readonly lblRawMaterials: Locator;
+    readonly btnAddRawMaterial: Locator;
+    readonly btnImportRawMaterial: Locator;
+    readonly btnExportRawMaterial: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.lblRawMaterial = page.getByRole('button', {name: 'Raw Materials', exact: true});
+        this.lblRawMaterials = page.getByRole('link', { name: 'Raw Materials', exact: true });
+        this.btnAddRawMaterial = page.getByRole('button', {name: 'Add Raw Material',exact: true });
+        this.btnImportRawMaterial = page.getByRole('button', { name: 'Import' }); 
+        this.btnExportRawMaterial = page.getByRole('button', { name: 'Export' });
+    }
+    async clickLblRawMaterial() {
+        await this.lblRawMaterial.click();
+    }
+    async ClicklblRawMaterials() {
+        await this.lblRawMaterials.click();
+
+    }
+    async clickbbtnAddRawMaterial(){
+        await this.btnAddRawMaterial.click();
+    }
+    async clickImport() {
+        await this.btnImportRawMaterial.click();
+    }
+    async clickExport() {
+        await  this.btnExportRawMaterial.click();
+    }
+}
