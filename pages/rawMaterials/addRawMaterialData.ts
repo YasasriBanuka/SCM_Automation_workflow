49f27bff22d19e3c253e34b65916570f9be79b45
YasasriBanuka <banukadias5@gmail.com>
2026-09-21 15:12:19 +0530

    Automate add new raw material data

---
+++ pages/rawMaterials/addRawMaterialData.ts
@@ -0,0 +1,68 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export class AddRawMaterialData {
+
+    readonly page: Page;
+    readonly txtItemCode: Locator;
+    readonly drpUOM: Locator;
+    readonly txtDescription: Locator;
+    readonly txtPurchaseLeadTime: Locator;
+    readonly txtQCLeadTime: Locator;
+    readonly btnaddMaterial: Locator;
+    readonly toastSuccessMessage: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+
+        this.txtItemCode = page.getByPlaceholder("RAW-MATERIAL-01");
+        this.drpUOM = page.getByRole("button", { name: "KG" });
+        this.txtDescription = page.getByPlaceholder("Raw material name");
+        this.txtPurchaseLeadTime = page.getByRole("spinbutton", { name: "7" });
+        this.txtQCLeadTime = page.getByRole("spinbutton", { name: "2" });
+        this.btnaddMaterial = page.getByRole('button', { name: 'Create Raw Material', exact: true });
+        this.toastSuccessMessage = page.getByRole('alert').filter({ hasText: 'Raw material created successfully.' })
+    }
+
+    async enterItemCode(
+
+        itemCode: string
+    ) {
+        await this.txtItemCode.fill(itemCode);
+    }
+
+    async selectUOM(
+
+        uom: string
+    ) {
+        await this.drpUOM.click();
+        await this.page.getByRole('option', { name: uom, exact: true }).click();
+    }
+
+    async enterDescription(
+
+        description: string
+    ) {
+        await this.txtDescription.fill(description);
+    }
+
+    async enterPurchaseLeadTime(
+
+        leadTime: string
+    ) {
+        await this.txtPurchaseLeadTime.fill(leadTime);
+    }
+
+    async enterQCLeadTime(
+
+        leadTime: string
+    ) {
+        await this.txtQCLeadTime.fill(leadTime);
+    }
+     async clickAddMaterial() {
+        await this.btnaddMaterial.click();
+    }
+
+    async verifyRawMaterial() {
+        await expect(this.toastSuccessMessage).toBeVisible();
+    }
+}
