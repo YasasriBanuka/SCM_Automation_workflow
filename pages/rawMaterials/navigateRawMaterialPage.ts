98739870fc5cbfbe36a662ef139a81513a59c502
YasasriBanuka <banukadias5@gmail.com>
2026-09-24 11:10:07 +0530

    Update and add verification for Navigation Raw Materials Details page

--- pages/rawMaterials/navigateRawMaterialPage.ts
+++ pages/rawMaterials/navigateRawMaterialPage.ts
@@ -8,16 +8,22 @@ export class navigateRawMaterialPage {
     readonly btnAddRawMaterial: Locator;
     readonly btnImportRawMaterial: Locator;
     readonly btnExportRawMaterial: Locator;
-   
+    readonly clickValue: Locator;
+    readonly clickDeactivateValue: Locator;
+   readonly clickCannotDeactivation: Locator;
+
 
 
     constructor(page: Page) {
         this.page = page;
-        this.lblRawMaterial = page.getByRole('button', {name: 'Raw Materials', exact: true});
+        this.lblRawMaterial = page.getByRole('button', { name: 'Raw Materials', exact: true });
         this.lblRawMaterials = page.getByRole('link', { name: 'Raw Materials', exact: true });
-        this.btnAddRawMaterial = page.getByRole('button', {name: 'Add Raw Material',exact: true });
-        this.btnImportRawMaterial = page.getByRole('button', { name: 'Import' }); 
+        this.btnAddRawMaterial = page.getByRole('button', { name: 'Add Raw Material', exact: true });
+        this.btnImportRawMaterial = page.getByRole('button', { name: 'Import' });
         this.btnExportRawMaterial = page.getByRole('button', { name: 'Export' });
+        this.clickValue = page.locator('tbody tr').filter({ hasText: 'RM_010' });
+        this.clickDeactivateValue = page.locator('tbody tr').filter({ hasText: 'RM_002' });
+        this.clickCannotDeactivation = page.locator('tbody tr').filter({ hasText: 'RM_001' });
     }
     async clickLblRawMaterial() {
         await this.lblRawMaterial.click();
@@ -25,13 +31,23 @@ export class navigateRawMaterialPage {
     async ClicklblRawMaterials() {
         await this.lblRawMaterials.click();
     }
-    async clickbbtnAddRawMaterial(){
+    async clickbbtnAddRawMaterial() {
         await this.btnAddRawMaterial.click();
     }
     async clickImport() {
         await this.btnImportRawMaterial.click();
     }
     async clickExport() {
-        await  this.btnExportRawMaterial.click();
+        await this.btnExportRawMaterial.click();
     }
+
+    async clickRMRow() {
+        await this.clickValue.click();
+    }
+    async clickDeactivatevalue() {
+        await this.clickDeactivateValue.click();
+    }
+    async clickCannotDeactivationvalue() {
+    await this.clickCannotDeactivation.click();
+}
 }
