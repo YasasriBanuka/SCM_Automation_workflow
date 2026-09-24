ef9987c4df5a33a78cd7a84a0b1914a2296b3cd0
YasasriBanuka <banukadias5@gmail.com>
2026-09-24 11:10:52 +0530

    Update test case workflow

--- tests/rawMaterialTest/rawMaterialTest.spec.ts
+++ tests/rawMaterialTest/rawMaterialTest.spec.ts
@@ -2,6 +2,7 @@ import { test } from '@playwright/test';
 import { navigateRawMaterialPage } from '../../pages/rawMaterials/navigateRawMaterialPage';
 import { adminLoginPage } from '../../pages/adminLoginPage/adminLoginPage';
 import { AddRawMaterialData } from '../../pages/rawMaterials/addRawMaterialData';
+import { changeRawMaterilaDetails } from '../../pages/rawMaterials/chnageAddedRawMatDetailsPage';
 
 
 test('TC_001 : Verify that an admin user can successfully create a new Raw Material with valid details.', async ({ page }) => {
@@ -131,7 +132,86 @@ test.only('TC_04 : Verify duplicate Item Code', async ({ page }) => {
 
 });
 
+test('TC_004: Verify admin can view and change added Raw Material Details', async ({ page }) => {
 
+  await page.goto('/signin');
 
+  const adminLogin = new adminLoginPage(page);
+  await adminLogin.enterUsername('admin.operations@companydemo.com');
+  await adminLogin.enterPassword('Admin@2026!');
+  await adminLogin.selectBranch();
+  await adminLogin.clickcheckbox();
+  await adminLogin.clickLogin();
+
+  const rawMaterialPage = new navigateRawMaterialPage(page);
+  // Navigate to Raw Materials
+  await rawMaterialPage.clickLblRawMaterial();
+  // Move to raw Material page section
+  await rawMaterialPage.ClicklblRawMaterials();
+  await rawMaterialPage.clickRMRow();
+
+  const chnageValue = new changeRawMaterilaDetails(page);
+  await chnageValue.verifySaveChangesDisabled();
+  await chnageValue.selectPCS();
+  await chnageValue.verifySaveChangesEnabled();
+
+  // Save changes
+  await chnageValue.clickSaveChanges();
+  await chnageValue.verifyRawMaterialUpdatedToast()
+
+  await page.waitForTimeout(4000);
+});
+
+test('TC_005: Verify admin can view and deactivate added Raw Material Details', async ({ page }) => {
+
+  await page.goto('/signin');
+
+  const adminLogin = new adminLoginPage(page);
+  await adminLogin.enterUsername('admin.operations@companydemo.com');
+  await adminLogin.enterPassword('Admin@2026!');
+  await adminLogin.selectBranch();
+  await adminLogin.clickcheckbox();
+  await adminLogin.clickLogin();
+
+  const rawMaterialPage = new navigateRawMaterialPage(page);
+  // Navigate to Raw Materials
+  await rawMaterialPage.clickLblRawMaterial();
+  // Move to raw Material page section
+  await rawMaterialPage.ClicklblRawMaterials();
+  await rawMaterialPage.clickRMRow();
+
+  const chnageValue = new changeRawMaterilaDetails(page);
+  await chnageValue.clickDeactivateButton();
+  await chnageValue.confirmDeactivate();
+  await chnageValue.verifyDeactivateSuccessMessage();
+
+  await page.waitForTimeout(4000);
+});
+
+test('TC_006: Verify admin cannot deactivate a raw material used in a product', async ({ page }) => {
+
+  await page.goto('/signin');
+
+  const adminLogin = new adminLoginPage(page);
+  await adminLogin.enterUsername('admin.operations@companydemo.com');
+  await adminLogin.enterPassword('Admin@2026!');
+  await adminLogin.selectBranch();
+  await adminLogin.clickcheckbox();
+  await adminLogin.clickLogin();
+
+  const rawMaterialPage = new navigateRawMaterialPage(page);
+  // Navigate to Raw Materials
+  await rawMaterialPage.clickLblRawMaterial();
+  // Move to raw Material page section
+  await rawMaterialPage.ClicklblRawMaterials();
+  await rawMaterialPage.clickCannotDeactivationvalue();
+
+  const chnageValue = new changeRawMaterilaDetails(page);
+  await chnageValue.clickDeactivateButton();
+  await chnageValue.confirmDeactivate();
+  await chnageValue.verifyCannotDeactivationMessage();
+
+  await page.waitForTimeout(4000);
+});
 
 
